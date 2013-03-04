cb5ecdc31f7ee6c642f89297d6064fe233eb27b9
ipsheeta <ipsheeta.furtado@gmail.com>
2013-03-04 13:39:59 -0800

    demo is go

--- launchdemo/slides/migrations/0002_auto__add_field_slide_point.py
+++ launchdemo/slides/migrations/0002_auto__add_field_slide_point.py
@@ -8,25 +8,23 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Slide'
-        db.create_table(u'slides_slide', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('graph_snippet', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal(u'slides', ['Slide'])
+        # Adding field 'Slide.point'
+        db.add_column(u'slides_slide', 'point',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=50, blank=True),
+                      keep_default=False)
 
 
     def backwards(self, orm):
-        # Deleting model 'Slide'
-        db.delete_table(u'slides_slide')
+        # Deleting field 'Slide.point'
+        db.delete_column(u'slides_slide', 'point')
 
 
     models = {
         u'slides.slide': {
             'Meta': {'object_name': 'Slide'},
-            'graph_snippet': ('django.db.models.fields.TextField', [], {}),
+            'graph_snippet': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'point': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
         }
     }
--- launchdemo/slides/models.py
+++ launchdemo/slides/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Slide(models.Model):
     title = models.CharField(max_length=200)
+    graph_snippet = models.TextField(blank=True)
+    point = models.CharField(max_length=50, blank=True)
 
     def __unicode__(self):
         return self.title
